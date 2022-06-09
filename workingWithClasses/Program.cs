7155214ca77c38b4dc0eff8916f9e5277d8b3384
Михаил Денисов <migelius.invesy@gmail.com>
2022-06-09 23:41:45 +0300

    Добавьте файлы проекта.

---
+++ workingWithClasses/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player();
+
+        player.ShowPlayerInformation();
+    }
+}
+
+class Player
+{
+    public string NamePlayer;
+    public int AgePlayer;
+    public string NameCharacter;
+    public string ClassCharacter;
+    public int LevelCharacter;
+
+    public Player()
+    {
+        NamePlayer = "Стёпа";
+        AgePlayer = 10;
+        NameCharacter = "МамкинНагибатель666";
+        ClassCharacter = "Мечник";
+        LevelCharacter = 94;
+    }
+
+    public void ShowPlayerInformation()
+    {
+        Console.WriteLine($"Имя игрока - {NamePlayer}" +
+            $"\nВозраст игрока - {AgePlayer} лет" +
+            $"\nИмя персонажа - {NameCharacter}"+
+            $"\nКласс персонажа - {ClassCharacter}" +
+            $"\nУровень персонажа - {LevelCharacter}");
+    }
+}
